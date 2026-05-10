53e813d6205b39e28ab0c65add098a2ebcab5948
Sujeet Pal <sujeetpal2606@gmail.com>
2026-05-10 20:28:36 +0530

    configured brevo mail service

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -3,29 +3,31 @@ const nodemailer = require("nodemailer");
 const mailSender = async (email, title, body) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
+      host: process.env.MAIL_HOST,
       port: 587,
-      secure: true,
+      secure: false,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
-      connectionTimeout: 10000,
     });
 
     const info = await transporter.sendMail({
-      from: "StudyNotion || by Sujeet Pal",
+      from: '"StudyNotion" <sujeetpal1818@gmail.com>',
       to: email,
       subject: title,
       html: body,
     });
 
-    console.log("Email sent successfully:", info.response);
+    console.log("Email sent:", info.messageId);
 
     return info;
+
   } catch (error) {
-    console.log("MAIL SENDER ERROR:", error);
+
+    console.log("MAIL ERROR:", error);
     throw error;
+
   }
 };
 
