cfa4d9e62fe58e069a26215aaa84ef33af7457d1
Sujeet Pal <sujeetpal2606@gmail.com>
2026-05-10 19:20:17 +0530

    fixed otp middleware

--- backend/models/OTP.js
+++ backend/models/OTP.js
@@ -1,47 +1,45 @@
-const mongoose = require('mongoose');
-const mailSender = require('../utils/mailSender');
+const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const OTPSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    otp: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-        expires: 5 * 60, // The document will be automatically deleted after 3 minutes of its creation time
-    }
+  email: {
+    type: String,
+    required: true,
+  },
 
+  otp: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    expires: 5 * 60,
+  },
 });
 
-//  function to send email
 async function sendVerificationEmail(email, otp) {
-    try {
-        const mailResponse = mailSender(email, 'Verification Email from StudyNotion', otp);
-        console.log('Email sent successfully to - ', email);
+  try {
+    const mailResponse = await mailSender(
+      email,
+      "Verification Email from StudyNotion",
+      otp
+    );
 
-    }
-    catch (error) {
-        console.log('Error while sending an email to ', email);
-        throw new error;
-    }
+    console.log("Email sent successfully to - ", email);
+  } catch (error) {
+    console.log("Error while sending an email to ", email);
+    throw error;
+  }
 }
 
-// pre middleware
-OTPSchema.pre('save', async (next) => {
-    // console.log("New document saved to database");
+OTPSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    await sendVerificationEmail(this.email, this.otp);
+  }
 
-    // Only send an email when a new document is created
-    if (this.isNew) {
-        await sendVerificationEmail(this.email, this.otp);
-    }
-    next();
-})
+  next();
+});
 
-
-
-module.exports = mongoose.model('OTP', OTPSchema);
+module.exports = mongoose.model("OTP", OTPSchema);
