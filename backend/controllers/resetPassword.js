4120996ec99d9bd677c73285f555bbbad24f2785
Sujeet Pal <sujeetpal2606@gmail.com>
2026-09-14 11:34:38 +0530

    Update backend functionality

--- backend/controllers/resetPassword.js
+++ backend/controllers/resetPassword.js
@@ -30,7 +30,7 @@ exports.resetPasswordToken = async (req, res) => {
 
 
         // create url
-        const url = `https://study-notion-edtech-platform-18.vercel.app/update-password/${token}`;
+        const url = `https://www.teachmate.tech/update-password/${token}`;
 
         // send email containing url
         await mailSender(email, 'Password Reset Link', `Password Reset Link : ${url}`);
